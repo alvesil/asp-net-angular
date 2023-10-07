8d63870378156cdeb36eb683bf4d278efb1de175
= <alvesil.eduardo.silva@gmail.com>
2023-10-07 17:39:08 -0300

    inserido componentes de perfil e dashboard

--- SmartSchool/src/app/app-routing.module.ts
+++ SmartSchool/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfessoresComponent } from './professores/professores.component';
 import { AlunosComponent } from './alunos/alunos.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'professores', component: ProfessoresComponent},
   { path: 'alunos', component: AlunosComponent},
-  { path: 'perfil', component: AlunosComponent},
-  { path: 'dashboard', component: AlunosComponent}
+  { path: 'perfil', component: PerfilComponent},
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
